112376c84d82b2c1901499e355b6b1d8bab525d5
Ted Ian Osias <osiastedian@gmail.com>
2018-03-03 11:08:33 +0800

    Upload to Github

---
+++ PathFind/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace PathFind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.findPathRadio = new System.Windows.Forms.RadioButton();
+            this.createWallRadio = new System.Windows.Forms.RadioButton();
+            this.pointLocation = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.diagonalCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 413);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.diagonalCheckBox);
+            this.groupBox1.Controls.Add(this.findPathRadio);
+            this.groupBox1.Controls.Add(this.createWallRadio);
+            this.groupBox1.Location = new System.Drawing.Point(668, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 135);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operation";
+            // 
+            // findPathRadio
+            // 
+            this.findPathRadio.AutoSize = true;
+            this.findPathRadio.Location = new System.Drawing.Point(6, 42);
+            this.findPathRadio.Name = "findPathRadio";
+            this.findPathRadio.Size = new System.Drawing.Size(70, 17);
+            this.findPathRadio.TabIndex = 1;
+            this.findPathRadio.TabStop = true;
+            this.findPathRadio.Text = "Find Path";
+            this.findPathRadio.UseVisualStyleBackColor = true;
+            // 
+            // createWallRadio
+            // 
+            this.createWallRadio.AutoSize = true;
+            this.createWallRadio.Location = new System.Drawing.Point(6, 19);
+            this.createWallRadio.Name = "createWallRadio";
+            this.createWallRadio.Size = new System.Drawing.Size(85, 17);
+            this.createWallRadio.TabIndex = 0;
+            this.createWallRadio.TabStop = true;
+            this.createWallRadio.Text = "Create Walls";
+            this.createWallRadio.UseVisualStyleBackColor = true;
+            // 
+            // pointLocation
+            // 
+            this.pointLocation.AutoSize = true;
+            this.pointLocation.Location = new System.Drawing.Point(668, 174);
+            this.pointLocation.Name = "pointLocation";
+            this.pointLocation.Size = new System.Drawing.Size(72, 13);
+            this.pointLocation.TabIndex = 2;
+            this.pointLocation.Text = "X: 100  Y:100";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // diagonalCheckBox
+            // 
+            this.diagonalCheckBox.AutoSize = true;
+            this.diagonalCheckBox.Location = new System.Drawing.Point(12, 74);
+            this.diagonalCheckBox.Name = "diagonalCheckBox";
+            this.diagonalCheckBox.Size = new System.Drawing.Size(104, 17);
+            this.diagonalCheckBox.TabIndex = 2;
+            this.diagonalCheckBox.Text = "Enable Diagonal";
+            this.diagonalCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 431);
+            this.Controls.Add(this.pointLocation);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Path Find";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton findPathRadio;
+        private System.Windows.Forms.RadioButton createWallRadio;
+        private System.Windows.Forms.Label pointLocation;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox diagonalCheckBox;
+    }
+}
+
---
+++ PathFind/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathFind
+{
+    public partial class Form1 : Form
+    {
+        Color PathColor = Color.Yellow;
+        Color WallColor = Color.Red;
+        Color PointerColor = Color.Blue;
+        Color GridColor = Color.Black;
+
+        int row = 0, col = 0;
+        int pictureBoxWith = 0, pictureBoxHeight = 0;
+        int cellWidth = 0, cellHeight = 0;
+        Dictionary<Cell, bool> walls;
+        Point pointerLocation;
+        Cell startLocation;
+        Cell endLocation;
+        List<Cell> path;
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pointerLocation = pictureBox1.PointToClient(System.Windows.Forms.Cursor.Position);
+            pointLocation.Text = pointerLocation.ToString();
+            path = GetPath(startLocation, endLocation, diagonalCheckBox.Checked);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGridLines(e.Graphics, row, col);
+            DrawWalls(e.Graphics);
+            DrawStartCell(e.Graphics);
+            if (!createWallRadio.Checked)
+            { 
+                DrawPath(e.Graphics, path);
+            }
+            //else
+            
+                DrawPointedCell(e.Graphics);
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            row = 20;
+            col = 20;
+            pictureBoxWith = pictureBox1.Width;
+            pictureBoxHeight = pictureBox1.Height;
+            cellWidth = pictureBoxWith / col;
+            cellHeight = pictureBoxHeight / row;
+            walls = new Dictionary<Cell, bool>();
+            timer1.Start();            
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (createWallRadio.Checked)
+            {
+                if (!AddWall(pointerLocation))
+                    RemoveWall(pointerLocation);
+
+            }
+            else if (findPathRadio.Checked)
+            {
+                if (startLocation == null)
+                    startLocation = new Cell();
+                startLocation.Row = pointerLocation.Y / cellHeight;
+                startLocation.Col = pointerLocation.X / cellWidth;
+            }
+        }
+
+        private void DrawGridLines(Graphics graphics, int row, int col)
+        {
+            int width = pictureBoxWith / col;
+            int height = pictureBoxHeight / row;
+            Pen gridPen = new Pen(GridColor, 2);
+            Point start = new Point();
+            Point end = new Point();
+            for (int i = 0; i < row; i++)
+            {
+                start.X = 0;
+                start.Y += height;
+                end.X = pictureBoxWith;
+                end.Y += height;
+                graphics.DrawLine(gridPen, start, end);
+            }
+            start = new Point();
+            end = new Point();
+            for (int i = 0; i < col; i++)
+            {
+                start.X += width;
+                start.Y = 0;
+                end.X += width;
+                end.Y = pictureBoxHeight;
+                graphics.DrawLine(gridPen, start, end);
+            }
+        }
+
+        private void DrawPointedCell(Graphics graphics)
+        {
+            int rowLoc = pointerLocation.Y / cellHeight;
+            int colLoc = pointerLocation.X / cellWidth;
+            if (endLocation == null)
+                endLocation = new Cell();
+            endLocation.Row = rowLoc;
+            endLocation.Col = colLoc;
+            if (!IsOutOfBounds(rowLoc, colLoc))
+            {
+                SolidBrush brush = new SolidBrush(this.PointerColor);
+                Rectangle rect = new Rectangle();
+                rect.Y = rowLoc * cellHeight;
+                rect.X = colLoc * cellWidth;
+                rect.Width = cellWidth;
+                rect.Height = cellHeight;
+                graphics.FillRectangle(brush, rect);
+            }
+        }
+
+        private void DrawWalls(Graphics graphics)
+        {
+            SolidBrush brush = new SolidBrush(WallColor);
+            Rectangle rect = new Rectangle();
+            rect.Width = cellWidth;
+            rect.Height = cellHeight;
+            foreach (Cell key in walls.Keys)
+            {
+                rect.Y = key.Row * cellHeight;
+                rect.X = key.Col * cellWidth;
+                graphics.FillRectangle(brush, rect);
+            }
+        }
+
+        private void DrawStartCell(Graphics graphics)
+        {
+            if(startLocation != null)
+            { 
+                SolidBrush brush = new SolidBrush(PathColor);
+                Rectangle rect = new Rectangle();
+                rect.Width = cellWidth;
+                rect.Height = cellHeight;
+                rect.Y = startLocation.Row * cellHeight;
+                rect.X = startLocation.Col * cellWidth;
+                graphics.FillRectangle(brush, rect);
+            }
+        }
+
+        private void DrawPath(Graphics graphics, List<Cell> list)
+        {
+            if (path == null)
+                return;
+            
+            SolidBrush brush = new SolidBrush(PathColor);
+            Rectangle rect = new Rectangle();
+            rect.Width = cellWidth;
+            rect.Height = cellHeight;
+            foreach (Cell cell in list)
+            {
+                rect.Y = cell.Row * cellHeight;
+                rect.X = cell.Col * cellWidth;
+                graphics.FillRectangle(brush, rect);
+            }
+        }
+
+        private bool AddWall(Point pointer)
+        {
+            Cell cell = new Cell();
+            cell.Row = pointer.Y / cellHeight;
+            cell.Col = pointer.X / cellWidth;
+            if (walls.ContainsKey(cell))
+                return false;
+            walls[cell] = true;
+            return true;
+        }
+        private bool RemoveWall(Point pointer)
+        {
+            Cell cell = new Cell();
+            cell.Row = pointer.Y / cellHeight;
+            cell.Col = pointer.X / cellWidth;
+            
+            if (walls.ContainsKey(cell))
+            { 
+                walls.Remove(cell);
+                return true;
+            }
+            return false;
+        }
+
+        private List<Cell> GetPath(Cell start, Cell end, bool diagonal = false)
+        {
+            if (start == null)
+                return null;
+            if (walls.ContainsKey(end))
+                return null;
+            Queue<Cell> queue = new Queue<Cell>();
+            Dictionary<Cell, Cell> visited = new Dictionary<Cell, Cell>();
+            queue.Enqueue(start);
+            bool found = false;
+            Cell currentCell = null;
+            while (!found && queue.Count > 0)
+            {
+                currentCell = queue.Dequeue();
+                List<Cell> adjacent = (diagonal)?currentCell.AdjacentWithDiagonal: currentCell.Adjacent;
+                foreach (Cell cell in adjacent)
+                {
+                    if (visited.ContainsKey(cell))
+                        continue;
+                    if (walls.ContainsKey(cell))
+                        continue;
+                    if (IsOutOfBounds(cell.Row, cell.Col))
+                        continue;
+                    visited[cell] = currentCell;
+                    if (cell.Equals(end))
+                    {
+                        found = true;
+                        break;
+                    }                    
+                    queue.Enqueue(cell);
+                }
+            }
+            if(found)
+            {
+                List<Cell> path = new List<Cell>();
+                Cell current = end;
+                while (!current.Equals(start))
+                {
+                    path.Add(current);
+                    current = visited[current];
+                }
+                return path.ToList();
+            }
+            return null;
+        }
+
+        private bool IsOutOfBounds(int rowLoc, int colLoc)
+        {
+            return !(rowLoc < this.row &&
+                rowLoc >= 0 &&
+                colLoc < this.col &&
+                colLoc >= 0);
+        }
+
+        class Cell
+        {
+            public Cell() { }
+            public Cell(int row, int col)
+            {
+                Row = row;
+                Col = col;
+            }
+            public int Row { get; set; }
+            public int Col { get; set; }
+            public Cell Left {
+                get {
+                    return new Cell(Row - 1, Col);
+                }
+            }
+            public Cell Right
+            {
+                get
+                {
+                    return new Cell(Row + 1, Col);
+                }
+            }
+            public Cell Top
+            {
+                get
+                {
+                    return new Cell(Row , Col - 1);
+                }
+            }
+            public Cell Bottom
+            {
+                get
+                {
+                    return new Cell(Row , Col + 1);
+                }
+            }
+            public Cell TopLeft
+            {
+                get
+                {
+                    return new Cell(Row - 1, Col - 1);
+                }
+            }
+            public Cell TopRight
+            {
+                get
+                {
+                    return new Cell(Row - 1, Col + 1);
+                }
+            }
+            public Cell BottomLeft
+            {
+                get
+                {
+                    return new Cell(Row + 1, Col - 1);
+                }
+            }
+            public Cell BottomRight
+            {
+                get
+                {
+                    return new Cell(Row + 1, Col + 1);
+                }
+            }
+            public List<Cell> Adjacent {
+                get
+                {
+                    List<Cell> list = new List<Cell>();
+                    list.Add(Left);
+                    list.Add(Right);
+                    list.Add(Top);
+                    list.Add(Bottom);
+                    return list;
+                }
+            }
+            public List<Cell> AdjacentWithDiagonal
+            {
+                get
+                {
+                    List<Cell> list = Adjacent;
+                    list.Add(TopLeft);
+                    list.Add(TopRight);
+                    list.Add(BottomLeft);
+                    list.Add(BottomRight);
+                    return list;
+                }
+            }
+
+            public override bool Equals(Object obj)
+            {
+                if (obj is Cell)
+                {
+                    Cell temp = (Cell)obj;
+                    return temp.Row == Row && temp.Col == Col;
+                }
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                var hashCode = 1084646500;
+                hashCode = hashCode * -1521134295 + Row.GetHashCode();
+                hashCode = hashCode * -1521134295 + Col.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
